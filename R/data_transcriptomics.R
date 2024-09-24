e280ea6687310d6191b94254b190f16609d62690
Stefano Mangiola <mangiolastefano@gmail.com>
2024-09-24 14:38:25 +0200

    improve dependencies

--- R/data_transcriptomics.R
+++ R/data_transcriptomics.R
@@ -1,75 +1,94 @@
 #' seurat_obj
 #'
-#' A Seurat dataset of single cell RNA sequencing data
+#' A Seurat dataset containing single-cell RNA sequencing data.
+#' 
+#' This Seurat object contains pre-processed single-cell RNA-seq data that can be used 
+#' for downstream analysis such as clustering, differential expression, and visualization.
 #'
-#'
-#' @format  A Seurat object.
+#' @format A `Seurat` object.
 #' @usage data(seurat_obj)
+#' @importFrom Seurat Seurat
 "seurat_obj"
 
 #' gate_seurat_obj
 #'
-#' Coordinates for a gate interactively drawn using tidygate
+#' Coordinates for a gate interactively drawn using `tidygate` on a Seurat object.
+#' 
+#' These x, y coordinates represent a manually defined gate used to subset specific populations
+#' from a Seurat object.
 #'
-#'
-#' @format  A list containing x,y coordinates for one gate
+#' @format A list containing x, y coordinates for one gate.
 #' @usage data(gate_seurat_obj)
 "gate_seurat_obj"
 
 #' seurat_obj_UMAP3
 #'
-#' A Seurat dataset of single cell RNA sequencing data with 3 UMAP dimesions
+#' A Seurat dataset of single-cell RNA sequencing data with 3 UMAP dimensions.
 #'
+#' This Seurat object contains three-dimensional UMAP embeddings for visualizing cell populations
+#' in a reduced-dimensional space.
 #'
-#' @format  A Seurat object.
+#' @format A `Seurat` object.
 #' @usage data(seurat_obj_UMAP3)
+#' @importFrom Seurat Seurat
 "seurat_obj_UMAP3"
 
 #' pseudo_bulk
 #'
-#' A SummarizedExperiment object
+#' A `SummarizedExperiment` object containing pseudo-bulk RNA sequencing data.
 #'
-#' @description
-#' This object was saved only because leading to a strange failure of the github action, 
-#' while working perfectly in the local environment
-#' 
+#' This object was created for benchmarking purposes, representing a pseudo-bulk aggregation 
+#' of single-cell data, saved due to a specific issue with GitHub actions while functioning 
+#' correctly in local environments.
 #'
-#' @format  A SummarizedExperiment object.
+#' @format A `SummarizedExperiment` object.
 #' @usage data(pseudo_bulk)
+#' @importFrom SummarizedExperiment SummarizedExperiment
 "pseudo_bulk"
 
 #' sce_obj
 #'
-#' A sce dataset of single cell RNA sequencing data
+#' A `SingleCellExperiment` dataset containing single-cell RNA sequencing data.
 #'
+#' This SingleCellExperiment (SCE) object contains pre-processed single-cell RNA-seq data 
+#' ready for downstream analysis, such as clustering or differential expression analysis.
 #'
-#' @format  A sce object.
+#' @format A `SingleCellExperiment` object.
 #' @usage data(sce_obj)
+#' @importFrom SingleCellExperiment SingleCellExperiment
 "sce_obj"
 
 #' gate_sce_obj
 #'
-#' Coordinates for a gate interactively drawn using tidygate
+#' Coordinates for a gate interactively drawn using `tidygate` on a SingleCellExperiment object.
+#' 
+#' These x, y coordinates represent a manually defined gate used to subset specific populations
+#' from a SingleCellExperiment object.
 #'
-#'
-#' @format  A list containing x,y coordinates for one gate
+#' @format A list containing x, y coordinates for one gate.
 #' @usage data(gate_sce_obj)
 "gate_sce_obj"
 
 #' sce_obj_UMAP3
 #'
-#' A sce dataset of single cell RNA sequencing data with 3 UMAP dimesions
+#' A `SingleCellExperiment` dataset of single-cell RNA sequencing data with 3 UMAP dimensions.
 #'
+#' This SCE object contains three-dimensional UMAP embeddings, which allow visualization 
+#' of cell populations in reduced-dimensional space.
 #'
-#' @format  A sce object.
+#' @format A `SingleCellExperiment` object.
 #' @usage data(sce_obj_UMAP3)
+#' @importFrom SingleCellExperiment SingleCellExperiment
 "sce_obj_UMAP3"
 
 #' theme_multipanel
 #'
-#' A file holding a theme object.
+#' A ggplot2 theme object designed for creating multipanel plots.
+#' 
+#' This theme can be applied to ggplot2-based visualizations to standardize the look and feel 
+#' of multipanel figures, ensuring consistency across plots.
 #'
-#'
-#' @format theme
+#' @format A `ggplot2` theme.
 #' @usage data(theme_multipanel)
+#' @importFrom ggplot2 theme
 "theme_multipanel"
